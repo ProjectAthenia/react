8abce83433745c8f1d6b1631abda96a64f69214c
Bryce Meyer <bryce13950@gmail.com>
2024-03-28 22:27:35 +0100

    passed tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,9 +4,7 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 import {  configure } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 import './testing/wrappers'
-
-require('matchmedia-polyfill');
 
 configure({adapter: new Adapter()});
--- src/testing/wrappers.tsx
+++ src/testing/wrappers.tsx
@@ -3,9 +3,3 @@ import Adapter from '@cfaester/enzyme-adapter-react-18';
 
 configure({adapter: new Adapter()});
 
-jest.mock('swiper/react', () => ({
-    Swiper: ({ children }: any) => <div data-testid="Swiper-testId">{children}</div>,
-    SwiperSlide: ({ children }: any) => (
-        <div data-testid="SwiperSlide-testId">{children}</div>
-    ),
-}))
