5bc789d7830001cd769b2cf0727f55145df1869a
Bryce Meyer <bryce13950@gmail.com>
2025-05-20 19:07:26 +0200

    remvoed post stuff

--- src/pages/Home/route-helpers.tsx
+++ src/pages/Home/route-helpers.tsx
@@ -1,26 +1,25 @@
-import React, {ReactElement} from 'react';
-import PostPage from './Post';
-import {Route} from 'react-router';
+import React, { ReactElement } from 'react';
+import { Route } from 'react-router-dom';
 
 interface SubRoute {
     routePath: string,
     page: ReactElement,
 }
+
 const SubRoutes: SubRoute[] = [
     {
-        routePath: '/post/:postId',
-        page: <PostPage/>,
+        routePath: '/',
+        page: <div>Home</div>,
     },
-]
-
+    // ... other routes ...
+];
 
 export const buildSubRoutes = (baseUrl: string): JSX.Element[] => {
     return SubRoutes.map(i =>
         <Route
             key={`${baseUrl}${i.routePath}`}
             path={`${baseUrl}${i.routePath}`}
-            exact
             render={() => i.page}
         />
-    )
-}
+    );
+};
