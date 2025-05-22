342c4eea140aa66171e0226d1328bd227ea1ed00
Bryce Meyer <bryce13950@gmail.com>
2025-05-22 18:49:45 +0200

    cleaned up test

--- src/util/collection-utils.test.ts
+++ src/util/collection-utils.test.ts
@@ -2,47 +2,19 @@ import { isInCollection } from './collection-utils';
 import { CollectionItemsContextState } from '../contexts/CollectionItemsContext';
 import { HasType } from '../models/has-type';
 import type CollectionItem from '../models/user/collection-items';
+import { mockPagination } from '../test-utils/mocks/pagination';
 
 describe('collection-utils', () => {
     describe('isInCollection', () => {
         const mockItem: HasType = { id: 1, type: 'item' };
         const mockCollectionId = 123;
         
-        const createMockContextState = (items: Array<{ item_id: number; item_type: string }>) => ({
+        const createMockContextState = (items: Array<{ item_id: number; item_type: string }>) => mockPagination<CollectionItem>({
             loadedData: items.map((item, index) => ({
                 ...item,
                 collection_id: mockCollectionId,
                 order: index + 1
-            })) as CollectionItem[],
-            hasAnotherPage: false,
-            initialLoadComplete: true,
-            initiated: true,
-            noResults: false,
-            page: 1,
-            perPage: 20,
-            total: items.length,
-            totalPages: 1,
-            loading: false,
-            error: null,
-            filters: {},
-            sort: {},
-            search: '',
-            selectedItems: new Set(),
-            selectedAll: false,
-            selectedCount: 0,
-            selectedPages: new Set(),
-            selectedPagesCount: 0,
-            selectedPagesTotal: 0,
-            selectedPagesTotalCount: 0,
-            selectedPagesTotalPages: 0,
-            selectedPagesTotalPerPage: 0,
-            selectedPagesTotalFilters: {},
-            selectedPagesTotalSort: {},
-            selectedPagesTotalSearch: '',
-            refreshing: false,
-            expands: {},
-            order: {},
-            filter: {}
+            })) as CollectionItem[]
         });
 
         it('should return false when collectionId is not provided', () => {
