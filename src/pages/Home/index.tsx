ba78d47cbd3fd5b03d75b1e03eb11a848300130b
Bryce Meyer <bryce13950@gmail.com>
2025-05-22 19:38:36 +0200

    emptied home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,32 +1,11 @@
 import React from 'react';
 import './index.scss';
 import Page from "../../components/Template/Page";
-import MeContextProvider, { MeContext } from '../../contexts/MeContext';
-import MyCollections from '../../components/Dashboard/MyCollections/index';
 
 const Home: React.FC = () => {
   return (
     <Page>
       <div className="home-content">
-        <h1>Welcome to Game Museum</h1>
-        
-        <MeContextProvider>
-          <MeContext.Consumer>
-            {({ me, isLoggedIn }) => (
-              <>
-                {isLoggedIn && me.id ? (
-                  <div className="dashboard-section">
-                    <MyCollections user={me} />
-                  </div>
-                ) : (
-                  <div className="welcome-section">
-                    <p>Please log in to view your collections and manage your game library.</p>
-                  </div>
-                )}
-              </>
-            )}
-          </MeContext.Consumer>
-        </MeContextProvider>
       </div>
     </Page>
   );
