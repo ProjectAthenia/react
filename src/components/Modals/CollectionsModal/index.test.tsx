8f7728c307c2b9c2eded8a609a812e860c1c8f74
Bryce Meyer <bryce13950@gmail.com>
2025-06-06 20:55:01 +0200

    fixed some errors

--- src/components/Modals/CollectionsModal/index.test.tsx
+++ src/components/Modals/CollectionsModal/index.test.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { screen } from '@testing-library/react';
 import CollectionsModal from './index';
 import { mockCollection } from '../../../test-utils/mocks/models/collection';
